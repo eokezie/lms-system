61de62a0ce7d06eb040a82d0d83a5f062f067b36
Charles Ejiegbu <charles.ejiegbu@gmail.com>
2026-03-04 11:55:28 +0100

    refactor: updated route

--- src/modules/lessons/lesson.routes.ts
+++ src/modules/lessons/lesson.routes.ts
@@ -1,14 +1,13 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import { authenticate, authorize } from "@/middleware/auth.middleware";
 import { USER_ROLES } from "../users/user.model";
 import { createMuxUploadHandler, muxWebhookHandler } from "./lesson.controller";
 
 const router = Router();
 
-router.use(authenticate);
-
 router.post(
 	"/",
+	authenticate,
 	authorize(USER_ROLES[1], USER_ROLES[2]),
 	// validate(createCourseSchema),
 	createMuxUploadHandler,
@@ -16,8 +15,7 @@ router.post(
 
 router.post(
 	"/webhook/mux",
-	authorize(USER_ROLES[1], USER_ROLES[2]),
-	// validate(createCourseSchema),
+	express.raw({ type: "application/json" }),
 	muxWebhookHandler,
 );
 

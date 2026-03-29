03e27053747422a699f690efb017af992312e1c3
Emeka Okezie <eokezie@nasdng.com>
2026-03-29 12:22:46 +0100

    feat: Made a fix

--- src/modules/discussions/discussion.routes.ts
+++ src/modules/discussions/discussion.routes.ts
@@ -14,29 +14,35 @@ import {
   getDiscussionThreadHandler,
   createDiscussionReplyHandler,
 } from "./discussion.controller";
+import { authorize } from "@/middleware/auth.middleware";
+import { USER_ROLES } from "../users/user.model";
 
 const router = Router({ mergeParams: true });
 
 router.get(
   "/course/:id",
+  authorize(USER_ROLES[0], USER_ROLES[1], USER_ROLES[2], USER_ROLES[3]) as any,
   validate(discussionCourseParamsSchema, "params"),
   validate(listDiscussionsQuerySchema, "query"),
   listDiscussionsHandler,
 );
 router.post(
   "/course/:id",
+  authorize(USER_ROLES[0], USER_ROLES[1], USER_ROLES[2], USER_ROLES[3]) as any,
   validate(discussionCourseParamsSchema, "params"),
   validate(createDiscussionSchema),
   createDiscussionHandler,
 );
 router.post(
   "/:threadId/replies",
+  authorize(USER_ROLES[0], USER_ROLES[1], USER_ROLES[2], USER_ROLES[3]) as any,
   validate(discussionThreadParamsSchema, "params"),
   validate(createReplySchema),
   createDiscussionReplyHandler,
 );
 router.get(
   "/:threadId",
+  authorize(USER_ROLES[0], USER_ROLES[1], USER_ROLES[2], USER_ROLES[3]) as any,
   validate(discussionThreadParamsSchema, "params"),
   validate(threadRepliesQuerySchema, "query"),
   getDiscussionThreadHandler,
